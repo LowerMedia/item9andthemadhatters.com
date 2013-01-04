5e096dc705ef0585cf34175ba5ad988c42620143
Pete Lower <pete.lower@gmail.com>
2013-01-04 10:01:42 -0600

    Adding easing slider to header.

--- wp-content/themes/i9-responsive-child-theme/home.php
+++ wp-content/themes/i9-responsive-child-theme/home.php
@@ -22,7 +22,7 @@ if ( !defined('ABSPATH')) exit;
  */
 ?>
 <?php get_header(); ?>
-		<?php if (function_exists("easing_slider")){ easing_slider(); }; ?>
+		
         <div id="featured" class="grid col-940">
         
         <div class="grid col-460">
